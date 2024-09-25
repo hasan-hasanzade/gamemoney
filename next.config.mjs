e971ee50619823fc801b8a5ba6866b5e38088606
ternyavsky <ilya.ternyavsky@yandex.ru>
2024-09-25 10:10:34 +0300

    standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,24 @@
 // next.config.mjs
 const nextConfig = {
+
+
+    output: 'standalone',
     webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              icon: true,
-              svgProps: { fill: 'currentColor' },
-            },
-          },
-        ],
-      });
-      return config;
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: [
+                {
+                    loader: '@svgr/webpack',
+                    options: {
+                        icon: true,
+                        svgProps: { fill: 'currentColor' },
+                    },
+                },
+            ],
+        });
+        return config;
     },
-  };
-  
-  export default nextConfig;
-  
+};
+
+export default nextConfig;
+
